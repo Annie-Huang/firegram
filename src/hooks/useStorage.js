0880a4e5cc1b847dc40ad7edeb763bf8925cbc41
Annie Huang <annie.hn@gmail.com>
2020-08-01 21:28:22 +1000

    Make call to get the percentage several times during the upload. Set the error if there is problem with upload and set the url of the file link in storage once finished upload.

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import {projectStorage} from "../firebase/config";
 
 const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
@@ -6,6 +7,19 @@ const useStorage = (file) => {
     const [url, setUrl] = useState(null);
 
     useEffect(() => {
-        
+        // references
+        const storageRef = projectStorage.ref(file.name);
+
+        storageRef.put(file).on('state_changed', (snap) => {
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage);
+        }, (err) => {
+            setError(err);
+        }, async () => {
+            const url = await storageRef.getDownloadURL();
+            setUrl(url);
+        });
     }, [file]);
+
+    return {progress, url, error}
 }
