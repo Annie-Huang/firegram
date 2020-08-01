df7db1f251bc94e708b390bb156dc8533e01a4b0
Annie Huang <annie.hn@gmail.com>
2020-08-01 18:45:36 +1000

    Add firebase configuration into config.js + "npm install firebase"

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAkAB1BZMM-EYDZUvzjWISvwRzf_s8VPHw",
+    authDomain: "ninja-firegram-de03f.firebaseapp.com",
+    databaseURL: "https://ninja-firegram-de03f.firebaseio.com",
+    projectId: "ninja-firegram-de03f",
+    storageBucket: "ninja-firegram-de03f.appspot.com",
+    messagingSenderId: "320963039733",
+    appId: "1:320963039733:web:0c15c4d2da0170f99fd0bc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
