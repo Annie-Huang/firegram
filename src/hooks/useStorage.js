591235fa89298679212cb2e1e2a3b81c08beaad5
Annie Huang <annie.hn@gmail.com>
2020-08-01 21:19:16 +1000

    Create a useStorage.js to make API call through useEffect.

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,11 @@
+import {useEffect, useState} from "react";
+
+const useStorage = (file) => {
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
+
+    useEffect(() => {
+        
+    }, [file]);
+}
