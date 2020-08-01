cb98966ee442d3cf045fe5a6256ecd6e2a28837e
Annie Huang <annie.hn@gmail.com>
2020-08-01 20:41:41 +1000

    Set up projectStorage, projectFirestore in config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase/app";
+import 'firebase/storage';
+import 'firebase/firestore';
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyAkAB1BZMM-EYDZUvzjWISvwRzf_s8VPHw",
@@ -10,3 +14,8 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export {projectStorage, projectFirestore};
