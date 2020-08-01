ad35877317749e2fdf868b4cf1768903cc6c81e0
Annie Huang <annie.hn@gmail.com>
2020-08-01 22:44:02 +1000

    Create useFirestore hook to make calls to the firebase database to retrieve information stored there.

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+import {projectFirestore} from "../firebase/config";
+
+const useFirestore = (collection) => {
+    const [docs, setDocs] = useState([]);
+
+    useEffect(() => {
+        projectFirestore.collection(collection)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot((snap) => {
+                // onSnapshot will constantly listening to the database.
+                let documents = [];
+
+                // doc.data has, e.g.
+                // createdAt: 1 August 2020 at 22:22:54 UTC+10
+                // url: "https://firebasestorage.googleapis.com/v0/b/ninja-firegram-de03f.appspot.com/o/roof.jpg?alt=media&token=6d0fd587-0503-4052-aa6c-d5d6898d19fe"
+                // doc.id has, e.g. zUCCSnyUW4B4pDc0L6ST
+                snap.forEach(doc => documents.push({...doc.data(), id: doc.id}));
+
+                setDocs(documents);
+            })
+    }, [collection]);
+
+    return { docs };
+}
