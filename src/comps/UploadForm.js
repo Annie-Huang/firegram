3a829d6016bfd6d734d43cdf69c992a6d23d8607
Annie Huang <annie.hn@gmail.com>
2020-08-01 20:59:45 +1000

    Upon selected a file, checked if it's a png or jpeg file. If it's, saved it to local variable file.

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = e => {
+        // console.log('changed');
+        let selected = e.target.files[0];
+
+        console.log(selected);
+        // File {name: "AngryBird.png", lastModified: 1498983641381, lastModifiedDate: Sun Jul 02 2017 18:20:41 GMT+1000 (Australian Eastern Standard Time),
+        // webkitRelativePath: "", size: 5339, type: "image/png" webkitRelativePath: ""}
+
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+        }
+    }
+
     return (
         <form>
-            <input type="file"/>
+            <input type="file" onChange={changeHandler}/>
         </form>
     );
 };
