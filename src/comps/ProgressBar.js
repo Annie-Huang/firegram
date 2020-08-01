c154b732af9e577236d831e2df4c54dd3813ac25
Annie Huang <annie.hn@gmail.com>
2020-08-01 21:37:46 +1000

    Create a ProgressBar component and include it into UploadForm.js

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const ProgressBar = ({file, setFile}) => {
+
+    return (
+        <div className="progress-bar">
+            progress
+        </div>
+    );
+};
+
+export default ProgressBar;
