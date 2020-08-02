c722fdd21aa6d71d86d5d8b789c0fbbf5ee52f86
Annie Huang <annie.hn@gmail.com>
2020-08-02 12:36:53 +1000

    Add layout animation which will smooth transit old images into new position when the new image is added. SUPER COOL!!

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -10,6 +10,7 @@ const ImageGrid = ({setSelectedImg}) => {
         <div className="img-grid">
             {docs && docs.map(doc => (
                 <motion.div className="img-wrap" key={doc.id}
+                    layout
                     whileHover={{opacity: 1}}
                     onClick={() => setSelectedImg(doc.url)}
                 >
