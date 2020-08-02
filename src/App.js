d2269e8b916a9e118b9e526ac1bd22780527a9ea
Annie Huang <annie.hn@gmail.com>
2020-08-02 10:55:23 +1000

    Create state when clicking the image, the url will pass from ImageGrid to App and then to Modal. And it will show the image for original size at the bottom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Title from './comps/Title';
 import UploadForm from "./comps/UploadForm";
 import ImageGrid from "./comps/ImageGrid";
 import Modal from "./comps/Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <Title/>
-      <UploadForm />
-      <ImageGrid />
-      <Modal />
-    </div>
-  );
+    const [selectedImg, setSelectedImg] = useState(null);
+
+    return (
+        <div className="App">
+            <Title/>
+            <UploadForm/>
+            <ImageGrid setSelectedImg={setSelectedImg} />
+            <Modal selectedImg={selectedImg} />
+        </div>
+    );
 }
 
 export default App;
