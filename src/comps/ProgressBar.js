ec3441685c95c95f7c97607e4dd2b939a545741f
Annie Huang <annie.hn@gmail.com>
2020-08-02 13:01:27 +1000

    Smooth progress on ProgressBar. THIS IS SUPER COOL TOO!

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import useStorage from "../hooks/useStorage";
+import {motion} from 'framer-motion'
 
 const ProgressBar = ({file, setFile}) => {
     const {url, progress} = useStorage(file);
@@ -15,8 +16,12 @@ const ProgressBar = ({file, setFile}) => {
         }
     }, [url, setFile]);
 
+    // <div className="progress-bar" style={{width: progress + '%'}}></div>
     return (
-        <div className="progress-bar" style={{width: progress + '%'}}></div>
+        <motion.div className="progress-bar"
+            initial={{width: 0}}
+            animate={{width: progress + '%'}}
+        ></motion.div>
     );
 };
 
